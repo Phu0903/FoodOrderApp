ba85482312f2f106dc77c7a8bd565841717eac5f
Phu0903 <71036328+Phu0903@users.noreply.github.com>
2021-04-08 14:04:22 +0700

    infoUser

--- backend_app/routes/users.js
+++ backend_app/routes/users.js
@@ -6,97 +6,103 @@ const { response } = require('../app');
 
 
 /*đăng kí*/
-router.post('/dangky', function(req, res, next) {
-  try{
-    var post_data = req.body;  
+router.post('/dangky', async (req, res, next) => {
+  try {
+    var post_data = req.body;
     var name = post_data.name;
     var email = post_data.email;
-    var phonenumber =post_data.phonenumber;
+    var phonenumber = post_data.phonenumber;
     var InfoUser = post_data.InfoUser;
-    var address = post_data.address 
-    if (!email  || !req.body.password )
-    {
-       return res
-       .status(400)
-       .json({success:false,message:"Username or password not empty"})
-    }
-    else if(!name)
-    {
+    var address = post_data.address
+    if (!email || !req.body.password) {
       return res
-      .status(400)
-      .json({success:false,message:"Name not empty"})
+        .status(400)
+        .json({ success: false, message: "Username or password not empty" })
     }
-    else if(!phonenumber)
-    {
+    else if (!name) {
       return res
-      .status(400)
-      .json({success:false,message:"Phonenumber not empty"})
+        .status(400)
+        .json({ success: false, message: "Name not empty" })
     }
-    else 
-    {
+    else if (!phonenumber) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Phonenumber not empty" })
+    }
+    else {
       var hashPassword = bcryptjs.hashSync(req.body.password, 10);
-    var innsertUser = {
-      '_email' : email,
-      '_password' : hashPassword,
-      '_name':name,
-      '_InfoUser':InfoUser,
-      '_PhoneNumber':phonenumber,
-      '_Address':address
+      var innsertUser = {
+        '_email': email,
+        '_password': hashPassword,
+        '_name': name,
+        '_InfoUser': InfoUser,
+        '_PhoneNumber': phonenumber,
+        '_Address': address
+      }
+      User.findOne({ '_email': email }).count(function (err, number) {
+        if (number != 0) {
+          res.send('Email already exists');
+        }
+        else {
+          var dulieu = new User(innsertUser);
+          dulieu.save();
+          res.send('Register success');
+        }
+
+
+      })
     }
-    User.findOne({'_email':email}).count(function(err,number){
-      if(number != 0)
-      {
-        res.send('Email already exists');
-      }
-      else {
-        var dulieu = new User(innsertUser);
-        dulieu.save();
-        res.send('Register success');
-      }
-    
-    
-    })
-  }
-  }catch (err) {
+  } catch (err) {
     res.status(500).json({
-        success: false,
-        message: err.message
+      success: false,
+      message: err.message
     });
-}
+  }
 });
 
 //đăng nhập
-router.post('/dangnhap', function(req, res, next) {
+router.post('/dangnhap', async (req, res, next) => {
   try {
     var post_data = req.body;
     var email = post_data.email;
-    User.findOne({'_email':email}).count(function(err,number){
-      if(number == 0)
-      {
+    User.findOne({ '_email': email }).count(function (err, number) {
+      if (number == 0) {
         res.json('Email not exists');
       }
       else {
-        User.findOne({'_email':email},function(error,data){
-          
-          if(!bcryptjs.compareSync(req.body.password, data._password))
-          {
+        User.findOne({ '_email': email }, function (error, data) {
+
+          if (!bcryptjs.compareSync(req.body.password, data._password)) {
             res.json('Wrong password or email');
-           
           }
-          else{
+          else {
             res.json('Login success');
           }
         })
       }
     })
-
-
   } catch (err) {
     res.status(500).json({
       success: false,
       message: err.message
-  });
+    });
   }
 });
 
+
+//getInforUser
+
+router.get('/inforUser/:email', async (req, res) => {
+  try {
+    var email = req.params.email;
+    User.find({ '_email': email }, function (err, dulieu) {
+      res.json(dulieu);
+    })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+})
 module.exports = router;
