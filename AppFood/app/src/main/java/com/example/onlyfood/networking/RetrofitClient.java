b427360b3d7de504d3d943e46382d961e0d7e0a4
Phu0903 <71036328+Phu0903@users.noreply.github.com>
2021-05-04 10:49:49 +0700

    update api

--- AppFood/app/src/main/java/com/example/onlyfood/networking/RetrofitClient.java
+++ AppFood/app/src/main/java/com/example/onlyfood/networking/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://192.168.1.7:3000";//Link api
+    private static final String BASE_URL = "https://app-food-using-project-java.herokuapp.com/";//Link api
     //192.168.1.7
    //http://10.45.152.14:3000/
     //http://10.0.135.172:3000/
